3bb793d7d803ecabac5182ea038468c1d61e3645
Nikita <tyumin000@gmail.com>
2024-01-13 16:17:09 +0300

    kek

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_uwsgi_websocket import GeventWebSocket
 app = Flask(__name__)
-websocket = GeventWebSocket()
+websocket = GeventWebSocket(app)
 from app import views
-websocket.init_app(app)
