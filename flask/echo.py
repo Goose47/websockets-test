1dedec52b0cdc78d5b03bb5de9596b74dd80feea
Nikita <tyumin000@gmail.com>
2024-01-25 19:21:03 +0300

    hmmm....

--- flask/echo.py
+++ flask/echo.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_uwsgi_websocket import WebSocket
 
 app = Flask(__name__)
@@ -7,12 +7,15 @@ ws = WebSocket(app)
 
 @app.route('/')
 def index():
-    return 'privet'
+    return 'prinvefas'
 
-@ws.route('/echo')
+@ws.route('/websocket')
 def echo(ws):
     while True:
         msg = ws.receive()
         if msg is not None:
             ws.send(msg)
         else: return
+
+if __name__ == '__main__':
+    app.run(debug=True, threads=16)
