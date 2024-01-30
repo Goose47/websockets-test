f2dbddd80d4bcffa1d9ae22e9a083bb8fe9cc43d
Nikita <tyumin000@gmail.com>
2024-01-30 21:13:55 +0300

    some fancy testststststst

--- fastapi/main.py
+++ fastapi/main.py
@@ -16,7 +16,7 @@ async def websocket_endpoint(websocket: WebSocket):
         await websocket.send_text(f"Message text was: {data}")
 
 
-@app.websocket("/")
+@app.websocket("/pidor")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
